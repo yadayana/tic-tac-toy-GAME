5eb6b79c8e23d5195f85def21071e9a2f6150918
yadayana <69514855+yadayana@users.noreply.github.com>
2020-08-11 17:25:39 +0300

    Create Tic tac toe.py

---
+++ Tic tac toe.py
@@ -0,0 +1,49 @@
+board = list(range(1, 10))
+
+def draw_board(board):
+    print("-------------")
+    for i in range(3):
+        print("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
+        print("-------------")
+
+def take_input(player_token):
+    valid = False
+    while not valid:
+        player_answer = input("Куда поставим " + player_token+"? ")
+        try:
+            player_answer = int(player_answer)
+        except:
+            print("Некорректный ввод. Вы уверены, что ввели число?")
+            continue
+        if player_answer >= 1 and player_answer <= 9:
+            if (str(board[player_answer-1]) not in "XO"):
+                board[player_answer-1] = player_token
+                valid = True
+            else:
+                print("Эта клеточка уже занята")
+        else:
+            print("Некорректный ввод. Введите число от 1 до 9 чтобы походить.")
+
+
+def main(board):
+    counter = 0
+    win = False
+    while not win:
+        draw_board(board)
+        if counter % 2 == 0:
+            take_input("X")
+        else:
+            take_input("O")
+        counter += 1
+        if counter > 4:
+            tmp = check_win(board)
+            if tmp:
+                print(tmp, "выиграл!")
+                win = True
+                break
+        if counter == 9:
+            print("Ничья!")
+            break
+    draw_board(board)
+
+main(board)
