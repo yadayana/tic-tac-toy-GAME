05b35e063d735ea5db2d785c40c7c696459704d8
yadayana <69514855+yadayana@users.noreply.github.com>
2020-08-11 17:31:15 +0300

    Update Tic tac toe.py
    Добавлена функция check_win(board)

--- Tic tac toe.py
+++ Tic tac toe.py
@@ -24,6 +24,12 @@ def take_input(player_token):
         else:
             print("Некорректный ввод. Введите число от 1 до 9 чтобы походить.")
 
+def check_win(board):
+    win_coord = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
+    for each in win_coord:
+        if board[each[0]] == board[each[1]] == board[each[2]]:
+            return board[each[0]]
+    return False
 
 def main(board):
     counter = 0
