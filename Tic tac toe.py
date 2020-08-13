f138807bf0abaf7c98f90ebdf83891f0dde7c5a3
yadayana <69514855+yadayana@users.noreply.github.com>
2020-08-13 16:45:30 +0300

    Update Tic tac toe.py

--- Tic tac toe.py
+++ Tic tac toe.py
@@ -1,3 +1,6 @@
+from Color_Console import *
+
+
 board = list(range(1, 10))
 
 def draw_board(board):
@@ -9,20 +12,20 @@ def draw_board(board):
 def take_input(player_token):
     valid = False
     while not valid:
-        player_answer = input("Куда поставим " + player_token+"? ")
+        player_answer = input("Куда поставим " + player_token + "?")
         try:
             player_answer = int(player_answer)
         except:
-            print("Некорректный ввод. Вы уверены, что ввели число?")
+            print(ctext("Некорректный ввод. Вы уверены, что ввели число?","red" , "black"))
             continue
         if player_answer >= 1 and player_answer <= 9:
             if (str(board[player_answer-1]) not in "XO"):
                 board[player_answer-1] = player_token
                 valid = True
             else:
-                print("Эта клеточка уже занята")
+                print(ctext("Эта клеточка уже занята","red" , "black"))
         else:
-            print("Некорректный ввод. Введите число от 1 до 9 чтобы походить.")
+            print(ctext("Некорректный ввод. Введите число от 1 до 9 чтобы походить.","red" , "black"))
 
 def check_win(board):
     win_coord = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
@@ -53,3 +56,6 @@ def main(board):
     draw_board(board)
 
 main(board)
+
+
+
